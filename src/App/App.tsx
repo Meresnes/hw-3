60042c55c082e5f973a128a4196a04225e5e888b
Meresnes <meresnes@yandex.ru>
2023-02-21 21:55:32 +0300

    first_commit

---
+++ src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi app</h1>
+    </div>
+  );
+}
+
+export default App;
