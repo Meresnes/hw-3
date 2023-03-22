13efab54184fa61a1e31268e2379a112ca111dc6
Meresnes <meresnes@yandex.ru>
2023-03-22 16:51:54 +0300

    modified:   src/index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import "regenerator-runtime";
 
 import "@config/configureMobX";
 import ReactDOM from "react-dom/client";
-import { HashRouter, BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import App from "./App/index";
 
